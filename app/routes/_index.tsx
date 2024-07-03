0c3d8cc37349e2cdbfa36f1ad446f3e366c83ae8
SamMcDonald828 <Sammcdonald828@gmail.com>
2024-07-03 14:53:57 -0400

    Not working yet

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,20 +10,20 @@ export default function Index() {
   const user = useOptionalUser();
   return (
     <main className="">
-      <WavyBackground className="max-w-4xl mx-auto pb-40">
-        <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl mt-28">
-          <span className="uppercase text-white drop-shadow-md">
+      <WavyBackground className="max-w-4xl pb-40 mx-auto">
+        <h1 className="text-6xl font-extrabold tracking-tight text-center sm:text-8xl lg:text-9xl mt-28">
+          <span className="text-white uppercase drop-shadow-md">
             Producers Space
           </span>
         </h1>
-        <p className="text-2xl mt-6 md:text-4xl text-white font-normal inter-var text-center">
+        <p className="mt-6 text-2xl font-normal text-center text-white md:text-4xl inter-var">
           The GitHub for Music Producers
         </p>
-        <div className="mx-auto mt-6 max-w-sm sm:flex sm:max-w-none sm:justify-center">
+        <div className="max-w-sm mx-auto mt-6 sm:flex sm:max-w-none sm:justify-center">
           {user ? (
             <Link
               to="/home"
-              className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-slate-700 shadow-sm hover:bg-slate-300 sm:px-8"
+              className="flex items-center justify-center px-4 py-3 text-base font-medium bg-white border border-transparent rounded-md shadow-sm text-slate-700 hover:bg-slate-300 sm:px-8"
             >
               View Home for {user.email}
             </Link>
@@ -31,13 +31,13 @@ export default function Index() {
             <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
               <Link
                 to="/join"
-                className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-slate-900 shadow-sm hover:bg-slate-900 hover:text-white sm:px-8"
+                className="flex items-center justify-center px-4 py-3 text-base font-medium bg-white border border-transparent rounded shadow-sm text-slate-900 hover:bg-slate-900 hover:text-white sm:px-8"
               >
                 Sign up
               </Link>
               <Link
                 to="/login"
-                className="flex items-center justify-center rounded-md bg-slate-700 px-4 py-3 font-medium text-white hover:bg-slate-400 hover:text-slate-900"
+                className="flex items-center justify-center px-4 py-3 font-medium text-white rounded bg-slate-700 hover:bg-slate-400 hover:text-slate-900"
               >
                 Log In
               </Link>
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -12,28 +12,52 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   return json({ noteListItems });
 };
 
-export default function NotesPage() {
+export default function HomePage() {
   const data = useLoaderData<typeof loader>();
   const user = useUser();
 
   return (
-    <div className="flex h-full min-h-screen flex-col">
+    <div className="flex flex-col h-full min-h-screen">
       {/**This is the navbar */}
-      <header className="flex items-center justify-between bg-slate-800 p-4 text-white">
+      <header className="flex items-center justify-between p-4 text-white bg-slate-800">
         <h1 className="text-2xl font-bold active:underline">
-          <Link to=".">Home</Link>
+          <NavLink
+            to="."
+            end
+            className={({ isActive }) =>
+              isActive ? "underline" : "opacity-60"
+            }
+          >
+            Home
+          </NavLink>
         </h1>
         <h1 className="text-2xl font-bold">
-          <Link to="/library">Library</Link>
+          <NavLink
+            to="/home/library"
+            end
+            className={({ isActive }) =>
+              isActive ? "underline" : "opacity-60"
+            }
+          >
+            Library
+          </NavLink>
         </h1>
         <h1 className="text-2xl font-bold">
-          <Link to="/Community">Community</Link>
+          <NavLink
+            to="/home/community"
+            end
+            className={({ isActive }) =>
+              isActive ? "underline" : "opacity-60"
+            }
+          >
+            Community
+          </NavLink>
         </h1>
         {/*<p>{user.email}</p>*/}
         <Form action="/logout" method="post">
           <button
             type="submit"
-            className="rounded bg-slate-600 px-4 py-2 text-blue-100 hover:bg-white hover:text-black active:bg-slate-400"
+            className="px-4 py-2 text-blue-100 rounded bg-slate-600 hover:bg-white hover:text-black active:bg-slate-400"
           >
             Logout
           </button>
@@ -41,7 +65,8 @@ export default function NotesPage() {
       </header>
       {/**This is the main window for the notes future=>homepage */}
       <main className="flex h-full bg-white">
-        <div className="h-full w-80 border-r bg-gray-50">
+        {/*
+        <div className="h-full border-r w-80 bg-gray-50">
           <Link to="new" className="block p-4 text-xl text-blue-500">
             + New Note
           </Link>
@@ -66,7 +91,7 @@ export default function NotesPage() {
               ))}
             </ol>
           )}
-        </div>
+                  </div> */}
 
         <div className="flex-1 p-6">
           <Outlet />
