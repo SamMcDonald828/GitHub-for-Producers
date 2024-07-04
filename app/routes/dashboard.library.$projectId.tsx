59705c32b73ac13b43087fb879d8913c843f1384
SamMcDonald828 <Sammcdonald828@gmail.com>
2024-07-04 18:50:47 -0400

    began new project function

---
+++ app/routes/dashboard.library.$projectId.tsx
@@ -0,0 +1,70 @@
+import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
+import {
+  Form,
+  isRouteErrorResponse,
+  useLoaderData,
+  useRouteError,
+} from "@remix-run/react";
+import invariant from "tiny-invariant";
+
+import { deleteProject, getProject } from "~/models/project.server";
+import { requireUserId } from "~/session.server";
+
+export const loader = async ({ params, request }: LoaderFunctionArgs) => {
+  const userId = await requireUserId(request);
+  invariant(params.noteId, "noteId not found");
+
+  const note = await getProject({ id: params.projectId, userId });
+  if (!note) {
+    throw new Response("Not Found", { status: 404 });
+  }
+  return json({ note });
+};
+
+export const action = async ({ params, request }: ActionFunctionArgs) => {
+  const userId = await requireUserId(request);
+  invariant(params.projectId, "noteId not found");
+
+  await deleteProject({ id: params.projectId, userId });
+
+  return redirect("/dashboard/library");
+};
+
+export default function ProjectDetailsPage() {
+  const data = useLoaderData<typeof loader>();
+
+  return (
+    <div>
+      <h3 className="text-2xl font-bold">{data.project.title}</h3>
+      <p className="py-6">{data.project.body}</p>
+      <hr className="my-4" />
+      <Form method="post">
+        <button
+          type="submit"
+          className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 focus:bg-blue-400"
+        >
+          Delete
+        </button>
+      </Form>
+    </div>
+  );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  if (error instanceof Error) {
+    return <div>An unexpected error occurred: {error.message}</div>;
+  }
+
+  if (!isRouteErrorResponse(error)) {
+    return <h1>Unknown Error</h1>;
+  }
+
+  if (error.status === 404) {
+    return <div>Note not found</div>;
+  }
+
+  return <div>An unexpected error occurred: {error.statusText}</div>;
+}
