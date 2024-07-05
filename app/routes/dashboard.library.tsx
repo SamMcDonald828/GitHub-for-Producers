4a76ac2ff36b01ec446a70424eb7c8147f26e7e3
SamMcDonald828 <Sammcdonald828@gmail.com>
2024-07-05 10:18:19 -0400

    Working Project new route

--- app/routes/dashboard.library.tsx
+++ app/routes/dashboard.library.tsx
@@ -1,80 +1,36 @@
-import { Link, NavLink, Outlet } from "@remix-run/react";
+import { Form, Link, NavLink, Outlet, useLoaderData } from "@remix-run/react";
 import { LoaderFunctionArgs, json } from "@remix-run/node";
 import { requireUserId } from "~/session.server";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "~/components/components/ui/card";
-import { Input } from "~/components/components/ui/input";
-import { Label } from "~/components/components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "~/components/components/ui/select";
 import { Button } from "~/components/components/ui/button";
+import ProjectDetailsPage from "./dashboard.library.$projectId";
+import { getProjectListItems } from "~/models/project.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const userId = await requireUserId(request);
-  return json({});
+  const projectListItems = await getProjectListItems({ userId });
+  return json({ projectListItems });
 };
 export default function LibraryPage() {
+  const data = useLoaderData<typeof loader>();
   return (
     <>
       <main className="flex h-full bg-white">
         <div className="w-48 h-full border-r bg-gray-50">
           <Link to="new" className="block p-4 text-xl text-blue-500">
-            + New Project
+            <Button type="submit">+ New Project</Button>
           </Link>
+          {data.projectListItems.map((project) => (
+            <ol key={project.id}>
+              <li>
+                <NavLink to={`${project.id}`}>
+                  <h1>{project.title}</h1>
+                  <p>{project.body}</p>
+                </NavLink>
+              </li>
+            </ol>
+          ))}
         </div>
-        <div className="">
-          <Card className="w-[350px]">
-            <CardHeader>
-              <CardTitle>Create project</CardTitle>
-              <CardDescription>
-                Deploy your new project in one-click.
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <form>
-                <div className="grid items-center w-full gap-4">
-                  <div className="flex flex-col space-y-1.5">
-                    <Label htmlFor="name">Name</Label>
-                    <Input
-                      className="rounded"
-                      id="name"
-                      placeholder="Name of your project"
-                    />
-                  </div>
-                  <div className="flex flex-col space-y-1.5">
-                    <Label htmlFor="framework">Framework</Label>
-                    <Select>
-                      <SelectTrigger id="framework" className="rounded">
-                        <SelectValue placeholder="Select" />
-                      </SelectTrigger>
-                      <SelectContent position="popper">
-                        <SelectItem value="next">Next.js</SelectItem>
-                        <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                        <SelectItem value="astro">Astro</SelectItem>
-                        <SelectItem value="nuxt">Nuxt.js</SelectItem>
-                      </SelectContent>
-                    </Select>
-                  </div>
-                </div>
-              </form>
-            </CardContent>
-            <CardFooter className="flex justify-between">
-              <Button>Cancel</Button>
-              <Button>Deploy</Button>
-            </CardFooter>
-          </Card>
-        </div>
+        <Outlet />
       </main>
     </>
   );
