88a4fa8eb8ed8718fcf26a287db39e9bb944a618
SamMcDonald828 <Sammcdonald828@gmail.com>
2024-08-09 12:08:02 -0400

    Fixed folder server

--- app/models/folder.server.ts
+++ app/models/folder.server.ts
@@ -1,9 +1,9 @@
 import type { Project, Folder } from "@prisma/client";
-
 import { prisma } from "~/db.server";
 
 export type { Folder } from "@prisma/client";
 
+// Function to get a specific folder by its id and related projectId
 export function getFolder({
   id,
   projectId,
@@ -16,14 +16,16 @@ export function getFolder({
   });
 }
 
-export function getFolderListItems({ folderId }: { folderId: Project["id"] }) {
+// Function to get all folder list items for a specific projectId
+export function getFolderListItems({ projectId }: { projectId: Project["id"] }) {
   return prisma.folder.findMany({
     where: { projectId },
-    select: { id: true, title: true, body: true},
+    select: { id: true, title: true, body: true },
     orderBy: { updatedAt: "desc" },
   });
 }
 
+// Function to create a new folder and associate it with a project
 export function createFolder({
   body,
   title,
@@ -44,30 +46,29 @@ export function createFolder({
   });
 }
 
+// Function to update an existing folder
 export function updateFolder({
+  id,
   body,
   title,
   projectId,
-}: Pick<Folder, "body" | "title"> & {
+}: Pick<Folder, "id" | "body" | "title"> & {
   projectId: Project["id"];
 }) {
   return prisma.folder.updateMany({
+    where: { id, projectId },
     data: {
       title,
       body,
-      project: {
-        connect: {
-          id: projectId,
-        },
-      },
     },
   });
 }
 
+// Function to delete a folder by its id and associated projectId
 export function deleteFolder({
   id,
   projectId,
-}: Pick<Folder, "id"> & { folderId: Project["id"] }) {
+}: Pick<Folder, "id"> & { projectId: Project["id"] }) {
   return prisma.folder.deleteMany({
     where: { id, projectId },
   });
