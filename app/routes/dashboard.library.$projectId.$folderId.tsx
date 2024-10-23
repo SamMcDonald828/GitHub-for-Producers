4b15f0d437ebbc25be61a8cd3c0ce02123b8ac20
SamMcDonald <Sammcdonald828@gmail.com>
2024-10-23 15:44:25 -0400

    folder Id page created

--- app/routes/dashboard.library.$projectId.$folderId.tsx
+++ app/routes/dashboard.library.$projectId.$folderId.tsx
@@ -4,6 +4,7 @@ import {
   Form,
   isRouteErrorResponse,
   Link,
+  NavLink,
   useLoaderData,
   useRouteError,
 } from "@remix-run/react";
@@ -14,7 +15,8 @@ import { deleteProject, getProject } from "~/models/project.server";
 import { requireUserId } from "~/session.server";
 
 export const loader = async ({ params, request }: LoaderFunctionArgs) => {
-    const projectId = await getProject({ id: params.projectId });
+    const userId = await requireUserId(request);
+    const projectId = await getProject({ id: params.projectId, userId });
 
     const folder = await getFolder({ id: params.folderId, projectId});
     if (!folder) {
@@ -32,3 +34,33 @@ export const action = async ({ params, request }: ActionFunctionArgs) => {
     return redirect("/dashboard/library");
 }
 
+export default function FolderDetailsPage() {
+    const data = useLoaderData<typeof loader>();
+
+    return (
+       <div>
+        <h3>{data.folder.title}</h3>
+        <Form method="post">
+            <input type="file">Upload a file/files</input>
+            <button type="submit">save</button>
+        </Form>
+        {data.folderFiles.map((file) => (
+            <li key={file.id}>
+                <NavLink
+                  className={({ isActive }) =>
+                    `block p-2 ${isActive ? "bg-slate-700 text-white rounded" : ""}`
+                  }
+                  to={file.id}
+                >
+                  {file.title}
+                  {/*<p>{file.body}</p>*/}
+                  {/*<p>{file.comments}</p>*/}
+                  {/*Audio file display would go here later*/}
+                  {/*Delete/upload replacement/merge*/}
+                </NavLink>
+            </li>
+        ))}
+       </div> 
+
+    );
+}
